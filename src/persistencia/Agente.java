d4cf9b38d70e7525621c688a7949013423b59ea9
sergio.fdz.gar <sergio.fdz.gar@53dbb4a9-1816-7768-3428-c507617fb8b3>
2013-10-01 08:44:50 +0000

    Share project "iso-practica-01" into "https://iso-practica-01.googlecode.com/svn"

---
+++ src/persistencia/Agente.java
@@ -0,0 +1,84 @@
+package persistencia;
+
+
+import java.awt.List;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Agente {
+	//instancia del agente
+    protected static Agente mInstancia=null;
+    //Conexion con la base de datos
+    protected static Connection mBD;
+	//Identificador ODBC de la base de datos
+    private static String url="jdbc:odbc:BD_Login";
+    //Driven para conectar con bases de datos Microsoft Access 
+    private static String driver="sun.jdbc.odbc.JdbcOdbcDriver";
+    
+    //Constructor
+    private Agente()throws Exception {
+    	conectar();
+    		
+    }
+    
+    //Implementacion del patron singleton
+    //Este patron de diseño permite implementar clases de las cuales
+    //solo existir una instancia
+    //http://es.wikipedia.org/wiki/Singleton
+     public static Agente getAgente() throws Exception{
+          if (mInstancia==null){
+          mInstancia=new Agente();
+        }
+        return mInstancia;
+     }
+ 
+    //Metodo para realizar la conexion a la base de datos 
+    private void conectar() throws Exception {
+         Class.forName(driver);
+         mBD=DriverManager.getConnection(url);
+    }
+
+    
+    //Metodo para desconectar de la base de datos
+    public void desconectar() throws Exception{
+    	mBD.close();
+    }
+
+    //Metodo para realizar una insercion en la base de datos
+    public int insert(String SQL) throws SQLException, Exception{ 
+     	conectar();
+    	PreparedStatement stmt = mBD.prepareStatement(SQL);
+    	int res=stmt.executeUpdate();
+    	stmt.close();
+    	desconectar();
+    	return res;
+    }
+    
+    //Metodo para realizar una eliminacion en la base de datos
+    public int delete(String SQL) throws SQLException,Exception{
+    	PreparedStatement stmt = mBD.prepareStatement(SQL);
+    	int res=stmt.executeUpdate();
+    	stmt.close();
+    	desconectar();
+    	return res;
+    }
+    
+    //Metodo para realizar una eliminacion en la base de datos
+    public int update(String SQL) throws SQLException,Exception{
+    	conectar();
+    	PreparedStatement stmt = mBD.prepareStatement(SQL);
+    	int res=stmt.executeUpdate();
+    	stmt.close();
+    	desconectar();
+    	return res;
+    }
+    
+    
+	public Vector<Object> select(String SQL) throws SQLException,Exception{
+		/*Metodo para realizar una busqueda o seleccion de informacion enla base de datos
+	    *El mŽtodo select develve un vector de vectores, donde cada uno de los vectores
+	    *que contiene el vector principal representa los registros que se recuperan de la base de datos.
+	    */	
+	}
+}
---
+++ src/presentacion/JFrameNuevoUsuario.java
@@ -0,0 +1,104 @@
+package presentacion;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import java.awt.Color;
+
+import javax.swing.JTextPane;
+
+import dominio.GestorUsuario;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class JFrameNuevoUsuario extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textFieldLogin;
+	private JTextField textFieldPassword;
+	private JTextPane textPane;
+
+	/**
+	 * Launch the application.
+	 
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrameNuevoUsuario frame = new JFrameNuevoUsuario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	*/
+	
+	/**
+	 * Create the frame.
+	 */
+	public JFrameNuevoUsuario() {
+		setTitle("Dar de alta a un nuevo usuario");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 420, 285);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblLogin = new JLabel("Password:");
+		lblLogin.setBounds(6, 81, 69, 16);
+		contentPane.add(lblLogin);
+		
+		JLabel label = new JLabel("Login:");
+		label.setBounds(6, 37, 69, 16);
+		contentPane.add(label);
+		
+		textFieldLogin = new JTextField();
+		textFieldLogin.setBounds(87, 31, 134, 28);
+		contentPane.add(textFieldLogin);
+		textFieldLogin.setColumns(10);
+		
+		textFieldPassword = new JTextField();
+		textFieldPassword.setColumns(10);
+		textFieldPassword.setBounds(87, 75, 134, 28);
+		contentPane.add(textFieldPassword);
+		
+		JButton btnAltaUsuario = new JButton("Alta usuario");
+		btnAltaUsuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//c—digo necesario para insertar un nuevo usuario en la base de datos.
+				//Se deber‡ comprobar que el usuario se ha insertado correctamente intentando 
+				//hacer el login			
+				//Se deber‡ reportar en el panel de estado el resultado de la inserci—n. Tanto su exito
+				//como cualquier excepci—n que pueda surgir.
+			}
+		});
+		btnAltaUsuario.setBounds(253, 76, 117, 29);
+		contentPane.add(btnAltaUsuario);
+		
+		JLabel label_1 = new JLabel("Estado");
+		label_1.setForeground(Color.RED);
+		label_1.setBounds(6, 126, 61, 16);
+		contentPane.add(label_1);
+		
+		textPane = new JTextPane();
+		textPane.setToolTipText("Panel para mostrar el restultado de la comprobaci\u00F3n de login o las excepciones lanzadas");
+		textPane.setEditable(false);
+		textPane.setBounds(6, 154, 407, 102);
+		contentPane.add(textPane);
+	}
+}
